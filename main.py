2eb441d6e414c822cbd57249ea20694efc786a15
海龍在天 <horyun.tw@gmail.com>
2026-03-16 14:40:56 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,31 @@
+import streamlit as st,io
 from styles import g,n
 
-def start():
-    user_text = input("Enter text: ")
-    user_style = input("Enter style (G/N): ").upper()
+st.set_page_config(page_title="BrandText", layout="centered")
+st.title("BrandText")
 
-    if user_style == "G":
-        final_image = g.render(user_text, 240)
-    elif user_style == "N":
-        final_image = n.render(user_text, 240)
+with st.container(border=True):
+    col1,col2 = st.columns([2,1])
+    text = col1.text_input("Enter text","BrandText")
+    style = col2.radio("Style",["Netflix","Google"],horizontal=True)
+    if style == "Netflix":
+        size = col1.slider("Font Size",50,200,150)
+        curve = col1.slider("Curve",0,100,30)
     else:
-        print("Error:// Invalid style.")
-        return
-    
-    save_path = f"brandtext_{user_text}.png"
-    final_image.save(save_path)
-    print(f"Saved to: {save_path}")
+        size = col1.slider("Font Size",50,200,150)
+        curve = 0
+if style == "Netflix":
+    img = n.render(text,size,curve)
+elif style == "Google":
+    img = g.render(text,size)
 
-if __name__ == "__main__":
-    start()
+st.image(img,use_container_width=True)
+
+buf = io.BytesIO()
+img.save(buf, format="PNG")
+st.download_button(
+    label=f"Download",
+    data=buf.getvalue(),
+    file_name=f"{text}.png",
+    mime="image/png"
+)
